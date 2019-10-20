f2581beb2bc11ab503dc0c719e83f64ce6d4de3a
Thor <thor@10.14.6>
2019-10-20 23:05:03 +0200

    second

--- src/simplefileiotestdriven/FileIn.java
+++ src/simplefileiotestdriven/FileIn.java
@@ -1,10 +1,21 @@
 package simplefileiotestdriven;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author thor
  */
 public class FileIn {
-	public void ReadCarFile(String filenameString) {
+	public String ReadCarFile(String filename) throws FileNotFoundException, IOException {
+		File fh = new File(filename);
+		FileReader fr = new FileReader(fh);
+		BufferedReader br = new BufferedReader(fr);
+		String foundline = br.readLine();
+		return foundline;
 	}
 }
--- test/simplefileiotestdriven/FileInTest.java
+++ test/simplefileiotestdriven/FileInTest.java
@@ -1,38 +1,42 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package simplefileiotestdriven;
 
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 /**
  *
  * @author thor
  */
 public class FileInTest {
-	
-	public FileInTest() {
-	}
+	String filename;
+	String foundline;
+	String expectedline;
+	FileIn fin;
 	
 	@Before
-	public void setUp() {
+	public void setUp() throws FileNotFoundException {
+		filename = "Data/test.csv";
+		fin = new FileIn();
 	}
-
+	
 	/**
 	 * Test of ReadCarFile method, of class FileIn.
 	 */
 	@Test
-	public void testReadCarFile() {
-		System.out.println("ReadCarFile");
-		String filenameString = "";
-		FileIn instance = new FileIn();
-		instance.ReadCarFile(filenameString);
+	public void testReadCarFile() throws FileNotFoundException, IOException {
+		foundline = fin.ReadCarFile(filename);
+		expectedline = "her bor kurt";
 		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
+		assertEquals(expectedline, foundline);
 	}
 	
 }
